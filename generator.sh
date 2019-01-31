43b120239e37f5bf05c9872b04163183bf19b332
Tamás András Horváth <htomy92@gmail.com>
2019-01-31 02:03:34 +0100

    Add more options for generator

--- generator.sh
+++ generator.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
 # Config
-PLUGIN_NAME_HYPEN="plugin-icetee"
+PLUGIN_NAME_HYPEN="plugin-name"
 PLUGIN_CREATOR_HYPEN="plugin-creator"
 PLUGIN_NAME_CAMEL_CASE="PluginName"
 PLUGIN_CREATOR_CAMEL_CASE="PluginCreator"
+PLUGIN_NAME="WordPress Plugin Boilerplate"
+PLUGIN_URI="http://example.com/plugin-name-uri/"
+PLUGIN_VERSION="1.0.0"
+PLUGIN_DESCRIPTION="This is a short description of what the plugin does. It's displayed in the WordPress admin area."
+PLUGIN_AUTHOR="Your Name or Your Company"
+PLUGIN_AUTHOR_URI="http://example.com/"
 
 # Generator
+function escape {
+  echo $(echo "$*" | sed 's/\//\\\//g' | sed 's/ /\\ /g' )
+}
+
+PLUGIN_NAME=$(escape $PLUGIN_NAME)
+PLUGIN_URI=$(escape $PLUGIN_URI)
+PLUGIN_AUTHOR=$(escape $PLUGIN_AUTHOR)
+PLUGIN_AUTHOR_URI=$(escape $PLUGIN_AUTHOR_URI)
+PLUGIN_VERSION=$(escape $PLUGIN_VERSION)
+
 cd ./plugin-name
 
-find . -type f -not -path "./vendor/*" -exec sed -i '' -e "s/plugin-name/$PLUGIN_NAME_HYPEN/" {} \;
-find . -type f -not -path "./vendor/*" -exec sed -i '' -e "s/plugin-creator/$PLUGIN_CREATOR_HYPEN/" {} \;
-find . -type f -not -path "./vendor/*" -exec sed -i '' -e "s/PluginName/$PLUGIN_NAME_CAMEL_CASE/" {} \;
-find . -type f -not -path "./vendor/*" -exec sed -i '' -e "s/PluginCreator/$PLUGIN_CREATOR_CAMEL_CASE/" {} \;
+# Metadata
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "Your Name or Your Company")/${PLUGIN_AUTHOR}/g" "{}" \;
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "WordPress Plugin Boilerplate")/${PLUGIN_NAME}/g" "{}" \;
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "http://example.com/plugin-name-uri/")/${PLUGIN_URI}/g" "{}" \;
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "http://example.com/")/${PLUGIN_AUTHOR_URI}/g" "{}" \;
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "This is a short description of what the plugin does. It's displayed in the WordPress admin area.")/${PLUGIN_DESCRIPTION}/g" "{}" \;
+find 'plugin-name.php' -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/$(escape "1.0.0")/${PLUGIN_VERSION}/g" "{}" \;
 
-find . -type f -not -path "./vendor/*" -name '*plugin-name*' | while read FILE ; do
+# Replaces
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/plugin-name/$PLUGIN_NAME_HYPEN/" {} \;
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/plugin-creator/$PLUGIN_CREATOR_HYPEN/" {} \;
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/PluginName/$PLUGIN_NAME_CAMEL_CASE/" {} \;
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -exec sed -i '' -e "s/PluginCreator/$PLUGIN_CREATOR_CAMEL_CASE/" {} \;
+
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -name '*plugin-name*' | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e "s/plugin-name/$PLUGIN_NAME_HYPEN/")" ;
     mv "${FILE}" "${newfile}";
 done
 
-find . -type f -not -path "./vendor/*" -name '*plugin-creator*' | while read FILE ; do
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -name '*plugin-creator*' | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e "s/plugin-creator/$PLUGIN_CREATOR_HYPEN/")" ;
     mv "${FILE}" "${newfile}";
 done
 
-find . -type f -not -path "./vendor/*" -name '*PluginName*' | while read FILE ; do
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -name '*PluginName*' | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e "s/PluginName/$PLUGIN_NAME_CAMEL_CASE/")" ;
     mv "${FILE}" "${newfile}";
 done
 
-find . -type f -not -path "./vendor/*" -name '*PluginCreator*' | while read FILE ; do
+find . -type f ! -path "./vendor/*" ! -path "composer.lock" -name '*PluginCreator*' | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e "s/PluginCreator/$PLUGIN_CREATOR_CAMEL_CASE/")" ;
     mv "${FILE}" "${newfile}";
 done
